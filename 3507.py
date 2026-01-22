2ae081391db644ed9265dd6c7636e26edd10a4d4
Piyush Yadav <125383648+PiyushYadav0021@users.noreply.github.com>
2026-01-22 23:52:24 +0530

    Add minimumPairRemoval method in Solution class
    Implement minimumPairRemoval method to count pairs removed.

---
+++ 3507.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumPairRemoval(self, nums: List[int]) -> int:
+        ordered = False
+        result = 0
+
+        while not ordered:
+            ordered = True 
+            minimum_value = float("inf")
+            minimum_index = -1
+
+            for i in range(len(nums)-1):
+                if nums[i+1] + nums[i] < minimum_value:
+                    minimum_value = nums[1+i] + nums[i]
+                    minimum_index = i
+                
+                if nums[i+1] < nums[i]:
+                    ordered = False
+            
+            if not ordered :
+                nums = nums[0 : minimum_index] + [minimum_value] + nums[minimum_index + 2:]
+                result +=1
+        return result
